5e9e75d6ed46b72b89e4c45c02245e9762574fac
clmntgbr <clement.goubier@gmail.com>
2020-06-27 12:27:37 +0200

    adding twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -18,12 +18,6 @@ class DefaultController extends AbstractController
         /* @var Weather $weather */
         $weather = $em->getRepository(Weather::class)->findBy([], ['id' => 'DESC'], 1, 0)[0];
 
-        dump($weather);
-        dump($weather->getTodayInformations());
-
-        foreach ($weather->getForecastInformations() as $forecastInformation) {
-            dump($forecastInformation);
-        }
-        die;
+        return $this->render('default/index.html.twig', []);
     }
 }
